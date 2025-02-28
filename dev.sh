41cbe9ccea8f6477f5daf7559b03b3a8690d1d99
Marouane Dbibih <marrouane.dbibih@gmail.com>
2025-02-28 23:18:05 +0000

    add the shutdown cmd to infra dev shell

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
+docker compose -f docker/compose.yml -f docker/compose.dev.yml down -v --remove-orphans
 docker compose -f docker/compose.yml -f docker/compose.dev.yml up --build
--- infra.sh
+++ infra.sh
@@ -1,1 +1,2 @@
+docker compose -f docker/compose.yml -v --remove-orphans
 docker compose -f docker/compose.yml up --build
