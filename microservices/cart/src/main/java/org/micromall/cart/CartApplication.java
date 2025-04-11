e67a9e29e3639e7e6d551458cf3c5d8bbda8c978
Marouane Dbibih <marouane.dbibih@outlook.com>
2025-04-11 16:22:18 +0100

    config redis on the cart

--- microservices/cart/src/main/java/org/micromall/cart/CartApplication.java
+++ microservices/cart/src/main/java/org/micromall/cart/CartApplication.java
@@ -2,8 +2,10 @@ package org.micromall.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CartApplication {
 
 	public static void main(String[] args) {
