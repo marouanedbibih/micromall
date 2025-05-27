802b9114dc9cb89271b15b6798d56c7381f1a8e1
Marouane Dbibih <marouane.dbibih@outlook.com>
2025-05-27 14:50:19 +0100

    Rebuild the user microservice

--- shutdown.sh
+++ shutdown.sh
@@ -1,1 +1,1 @@
-docker compose -f docker/compose.yml -f docker/compose.dev.yml down -v --remove-orphans
+docker compose -f docker/compose.yml -f docker/compose.dev.yml down
---
+++ user-service/src/main/java/org/micromall/userservice/config/KeycloakProperties.java
@@ -0,0 +1,47 @@
+package org.micromall.userservice.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "keycloak")
+public class KeycloakProperties {
+    
+    private String authServerUrl;
+    private String realm;
+    private String clientId;
+    private String clientSecret;
+
+    public String getAuthServerUrl() {
+        return authServerUrl;
+    }
+
+    public void setAuthServerUrl(String authServerUrl) {
+        this.authServerUrl = authServerUrl;
+    }
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public void setRealm(String realm) {
+        this.realm = realm;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+}
