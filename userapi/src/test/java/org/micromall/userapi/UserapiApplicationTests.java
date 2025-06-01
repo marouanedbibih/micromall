cbc8976bee7d5a03211843f3f218a09b9a9ab8f1
Marouane Dbibih <marouane.dbibih@outlook.com>
2025-06-01 14:13:24 +0100

    rebuild the user microservice

--- userapi/src/test/java/org/micromall/userapi/UserapiApplicationTests.java
+++ userapi/src/test/java/org/micromall/userapi/UserapiApplicationTests.java
@@ -1,13 +1,13 @@
-package org.micromall.userservice;
+package org.micromall.userapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class UserapiApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
 }
